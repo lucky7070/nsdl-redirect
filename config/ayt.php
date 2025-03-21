5cf9ad2492041f2d33d06df8c577eb94dd2b1c0f
Lucky Solanki <luckysolanki7070@gmail.com>
2025-03-21 13:13:35 +0530

    update

--- config/ayt.php
+++ config/ayt.php
@@ -5,6 +5,8 @@ return [
 	'pan-card-service' => [
 		'entityCode' 	=> 'ADIYOGIRD',
 		'jar-file' 		=> 'EntityRequestUtility.jar',
-		'url' 			=> 'https://test1.tin-nsdl.com/paam/redirectToFullFormApp.html',
+		// 'url' 			=> 'https://test1.tin-nsdl.com/paam/redirectToFullFormApp.html',
+		'url'			=> 'https://172.19.75.207/paam/redirectToFullFormApp.html',
+
 	]
 ];
